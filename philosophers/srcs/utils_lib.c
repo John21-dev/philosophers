e9e771170dba14ef1c1b657adc06d2c70c1bdc0b
John21 <66877991+John21-dev@users.noreply.github.com>
2024-08-17 12:54:39 +0200

    Add files via upload

---
+++ philosophers/srcs/utils_lib.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_lib.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmacera- <jmacera-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 11:43:43 by jmacera-          #+#    #+#             */
+/*   Updated: 2024/05/16 11:50:28 by jmacera-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	if (fd >= 0)
+		write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (fd >= 0)
+	{
+		while (s && *s)
+		{
+			write(fd, &*s, 1);
+			s++;
+		}
+	}
+}
+
+void	ft_putnbr_fd(long int ln, int fd)
+{
+	if (ln < 0)
+	{
+		ln *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (ln >= 10)
+	{
+		ft_putnbr_fd(ln / 10, fd);
+		ft_putnbr_fd(ln % 10, fd);
+	}
+	else
+	{
+		if (fd >= 0)
+			ft_putchar_fd(ln + 48, fd);
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
